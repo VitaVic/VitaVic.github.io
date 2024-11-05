ab8f031f2ec35192755e203095e5c133e65fe65d
vita <victor.strickmann@code.berlin>
2024-11-05 11:36:41 +0100

    Deleted old files.

--- se19/stickerSchema.mjs
+++
@@ -1,9 +0,0 @@
-const stickerSchema = new mongoose.Schema({
-    slug: { type: String, required: true, unique: true},
-    name: { type: String, required: true, unique: true},
-    priceInCents: { type: Number, required: true},
-    stockInUnits: { type: Number, required: true},
-    isInStock: { type: String, default: true, required: true},
-})
-
-const Sticker = mongoose.model('Sticker', stickerSchema)
